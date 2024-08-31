d148627a042b442abe4e7c7e6dba4f413b1348ce
Youssef Abdellaoui <kenicastrocantabria@gmail.com>
2024-08-31 21:42:52 +0200

    add Clerk auth functionality + add Liveblocks functionality + add styling & icons

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,16 +1,9 @@
-import { Editor } from '@/components/editor/Editor'
-import Header from '@/components/Header'
 import React from 'react'
 
 const Document = () => {
   return (
     <div>
-        <Header>
-            <div className='flex w-fit items-center justify-center gap-2'>
-            <h1 className='document-title'>The Document</h1>
-            </div>
-        </Header>
-        <Editor />
+        
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter as FontSans } from "next/font/google"
 import './globals.css'
 import { cn } from "@/lib/utils"
 import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+import Provider from "./Provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -16,15 +19,28 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { 
+          colorPrimary: '#4273e5', 
+          fontSize: '16px',
+        },
+      }}
+    
+    >
+      <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
           "min-h-screen font-sans antialiased",
           fontSans.variable
         )}
       >
-        {children}
+        <Provider>
+          {children}
+        </Provider>
       </body>
     </html>
+    </ClerkProvider>
   )
 }

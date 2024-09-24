8562e45c9103e7adf5fd42dc8bf96839c4b0e8dc
youssef abdellaoui <youssef.abdellaoui@wtt-campusone.com>
2024-09-24 10:28:24 +0200

    add loading animation to Add new document button

--- components/AddDocumentBtn.tsx
+++ components/AddDocumentBtn.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from './ui/button'
 import Image from 'next/image'
 import { createDocument } from '@/lib/actions/room.actions';
@@ -9,7 +9,10 @@ import { useRouter } from 'next/navigation';
 const AddDocumentBtn = ({ userId, email }: AddDocumentBtnProps) => {
 const router = useRouter();
 
+const [loading, setLoading] = useState(false);
+
   const AddDocumentHandler = async () => {
+    setLoading(true); // Set loading to true while creating the document
     try {
       const room = await createDocument({ userId, email });
       // Redirect to the new document, when created
@@ -17,17 +20,19 @@ const router = useRouter();
     } catch (error) {
       console.log(`Error creating document ${error}`);
     }
+    setLoading(false); // Set loading to false after creating the document
   }
 
   return (
-    <Button type='submit' onClick={AddDocumentHandler} className='gradient-blue flex gap-1 shadow-md p-4'>
+    <Button type='submit' onClick={AddDocumentHandler} className='gradient-blue flex gap-1 shadow-md p-4' disabled={loading}>
         <Image 
           src="/assets/icons/add.svg"
           alt='Add icon'
           width={24}
           height={24}
         />
-        <p className='hidden sm:block'>Start a blank document</p>
+        {loading ? 'Creating...' : <p className='hidden sm:block'>Start a blank document</p>}
+        
     </Button>
   )
 }
